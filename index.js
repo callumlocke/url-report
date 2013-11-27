bcf382a095564df160eb8e78ee250d2190c6b3d8
Callum Locke <callum.locke@gmail.com>
2013-11-27 15:04:38 +0000

    use full path to script in phantomjs command

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var exec = require('child_process').exec,
-    phantomjs = require('phantomjs');
+    phantomjs = require('phantomjs'),
+    path = require('path');
+
+var escape = function(cmd) {
+  return '"' + cmd.replace(/(["\s'$`\\])/g, '\\$1') + '"';
+};
 
 module.exports = {
   load: function (url, callback) {
-    exec(phantomjs.path + ' report.js "' + url + '"', function (err, stdout, stderr) {
+    var command = [
+      phantomjs.path,
+      escape(path.join(__dirname, 'report.js')),
+      escape(url)
+    ].join(' ');
+
+    // console.log(command);
+
+    exec(command, function (err, stdout, stderr) {
       if (err) throw err;
       callback(JSON.parse(stdout));
     });
