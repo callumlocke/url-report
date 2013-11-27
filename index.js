720882a62c08230ec3fb2effef7547f30bf6d3c5
Callum Locke <callum.locke@gmail.com>
2013-11-27 13:17:27 +0000

    remove console.log

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var exec = require('child_process').exec,
     phantomjs = require('phantomjs');
 
-console.log('phantomjs.path', phantomjs.path);
-
 module.exports = {
   load: function (url, callback) {
     exec(phantomjs.path + ' report.js "' + url + '"', function (err, stdout, stderr) {
